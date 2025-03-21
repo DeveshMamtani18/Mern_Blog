571276ad885394c3e4124a508b8c1c00c57f4b33
DeveshMamtani18 <deveshmamtani18@gmail.com>
2025-03-21 09:14:55 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Dashboard from'./pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 
 export default function App() {
   return (
-    <div className='bg-gray-700'>
-      Hello world
-    </div>
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/Dashboard' element={<Dashboard/>}/>
+        <Route path='/Signin' element={<Signin/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div>
+      I am Signin
+    </div>
+  )
+}
+
+export default Signin
